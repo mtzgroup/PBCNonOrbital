a2c97eaedb3167f58c0ec69111929a027f8872d5
Todd Martinez <toddjmartinez@gmail.com>
2024-07-29 16:34:28 -0700

    first commit

---
+++ periodic_nuclear_repulsion.h
@@ -0,0 +1,15 @@
+#ifndef PERIODIC_NUCLEAR_REPULSION_H_
+#define PERIODIC_NUCLEAR_REPULSION_H_
+
+#include "periodic_types.h"
+
+namespace PeriodicBox
+{
+  double PeriodicNuclearRepulsion(const double full_range_scale,
+                                  const double long_range_scale,
+                                  const PeriodicParameter periodic_parameter,
+                                  const int n_charge,
+                                  const double* charge_xyzc);
+}
+
+#endif
