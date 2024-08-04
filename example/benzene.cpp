0eb4e188f799ca73997915dcd8140e5bbca69400
Henry Wang <henrywang0142857@gmail.com>
2024-08-04 15:56:24 -0700

    Gradient working

--- example/benzene.cpp
+++ example/benzene.cpp
@@ -2,6 +2,8 @@
 #include "../src/helper.h"
 #include "../src/periodic_nuclear_repulsion.h"
 
+#include <string.h>
+
 int main()
 {
     PeriodicBox::LatticeInfo lattice;
@@ -22,11 +24,11 @@ int main()
     periodic_parameter.thresholds.periodic_orbtial_cutoff = NAN;
     periodic_parameter.thresholds.periodic_charge_cutoff_real = 1e-14;
     periodic_parameter.thresholds.periodic_charge_cutoff_reciprocal = 1e-14;
-    periodic_parameter.omega = 1.0;
-    printf("omega = %.5e\n", periodic_parameter.omega);
+    periodic_parameter.omega = 0.2;
+    printf("\nomega = %.5e\n", periodic_parameter.omega);
 
     const int n_atom = 12 * 2;
-    const double xyzc[n_atom * 4] = {
+    const double xyzc[n_atom * 4] {
          1.7150183861 / BohrToAng, 5.3416872000 / BohrToAng, 6.3725303821 / BohrToAng, 6.0,
          0.9676735997 / BohrToAng, 5.2813076400 / BohrToAng, 5.8222081416 / BohrToAng, 1.0,
          2.7131641240 / BohrToAng, 4.3516800000 / BohrToAng, 6.3186476562 / BohrToAng, 6.0,
@@ -56,7 +58,51 @@ int main()
     const double full_range_scale = 1.0;
     const double long_range_scale = 0.0;
     const double E = PeriodicBox::PeriodicNuclearRepulsion(full_range_scale, long_range_scale, periodic_parameter, n_atom, xyzc);
-    printf("E = %.10f\n", E);
+    printf("\nE = %.10f\n", E);
+
+    double* analytical_gradient = new double[n_atom * 3];
+    double* numerical_gradient  = new double[n_atom * 3];
+    memset(analytical_gradient, 0, n_atom * 3 * sizeof(double));
+    memset(numerical_gradient,  0, n_atom * 3 * sizeof(double));
+
+    PeriodicBox::PeriodicNuclearRepulsionGradient(full_range_scale, long_range_scale, periodic_parameter, n_atom, xyzc, analytical_gradient);
+
+    for (int i_atom = 0; i_atom < n_atom; i_atom++)
+        printf("Analytical gradient %2d  %15.10f  %15.10f  %15.10f\n",
+            i_atom, analytical_gradient[i_atom * 3 + 0], analytical_gradient[i_atom * 3 + 1], analytical_gradient[i_atom * 3 + 2]);
+
+    double* xyzc_copy = new double[n_atom * 4];
+    memcpy(xyzc_copy, xyzc, n_atom * 4 * sizeof(double));
+
+    const double dx = 1e-4;
+
+    for (int i_atom = 0; i_atom < n_atom; i_atom++)
+        for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+            xyzc_copy[i_atom * 4 + i_xyz] = xyzc[i_atom * 4 + i_xyz] + dx;
+            const double E_plus  = PeriodicBox::PeriodicNuclearRepulsion(full_range_scale, long_range_scale, periodic_parameter, n_atom, xyzc_copy);
+
+            xyzc_copy[i_atom * 4 + i_xyz] = xyzc[i_atom * 4 + i_xyz] - dx;
+            const double E_minus = PeriodicBox::PeriodicNuclearRepulsion(full_range_scale, long_range_scale, periodic_parameter, n_atom, xyzc_copy);
+
+            numerical_gradient[i_atom * 3 + i_xyz] = (E_plus - E_minus) / dx / 2.0;
+            xyzc_copy[i_atom * 4 + i_xyz] = xyzc[i_atom * 4 + i_xyz];
+        }
+
+    for (int i_atom = 0; i_atom < n_atom; i_atom++)
+        printf("Numerical gradient %2d  %15.10f  %15.10f  %15.10f\n",
+            i_atom, numerical_gradient[i_atom * 3 + 0], numerical_gradient[i_atom * 3 + 1], numerical_gradient[i_atom * 3 + 2]);
+
+    double max_abs_diff = 0.0;
+    for (int i_atom = 0; i_atom < n_atom; i_atom++)
+        for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+            const double abs_diff = fabs(analytical_gradient[i_atom * 3 + i_xyz] - numerical_gradient[i_atom * 3 + i_xyz]);
+            if (abs_diff > max_abs_diff) max_abs_diff = abs_diff;
+        }
+    printf("\nMax abs diff between analytical and numerical: %.5e\n", max_abs_diff);
+
+
+    delete[] analytical_gradient;
+    delete[] numerical_gradient;
 
     return 0;
 }
--- src/periodic_nuclear_repulsion.cpp
+++ src/periodic_nuclear_repulsion.cpp
@@ -5,6 +5,7 @@
 
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
 
 namespace PeriodicBox
 {
@@ -74,5 +75,107 @@ namespace PeriodicBox
     E_reciprocal *= (full_range_scale + long_range_scale) * 4.0 * M_PI / periodic_parameter.lattice.V_real;
 
     return E_real + E_reciprocal;
+  }
+
+  void PeriodicNuclearRepulsionGradient(const double full_range_scale,
+                                        const double long_range_scale,
+                                        const PeriodicParameter periodic_parameter,
+                                        const int n_charge,
+                                        const double* charge_xyzc,
+                                        double* gradient)
+  {
+    const double omega = periodic_parameter.omega;
+
+    const double nuclear_cutoff_real = get_nuclear_cutoff_distance_real(periodic_parameter.thresholds.periodic_charge_cutoff_real, 1.0, omega);
+    const double nuclear_cutoff_reciprocal = get_nuclear_cutoff_distance_reciprocal(periodic_parameter.thresholds.periodic_charge_cutoff_reciprocal, 4.0 * M_PI / periodic_parameter.lattice.V_real, omega);
+    // printf("nuclear_cutoff_real = %.5f, nuclear_cutoff_reciprocal = %.5f\n", nuclear_cutoff_real, nuclear_cutoff_reciprocal);
+
+    const std::vector<double3> reciprocal_vectors = PeriodicBox::compute_lattice_vectors_within_sphere_reciprocal(periodic_parameter.lattice.unit_cell, nuclear_cutoff_reciprocal);
+
+    double* dE_real       = new double[n_charge * 3];
+    double* dE_reciprocal = new double[n_charge * 3];
+    memset(dE_real,       0, n_charge * 3 * sizeof(double));
+    memset(dE_reciprocal, 0, n_charge * 3 * sizeof(double));
+
+    for (int i_charge = 0; i_charge < n_charge; i_charge++) {
+      const double q_i = charge_xyzc[i_charge * 4 + 3];
+      const double P_i[3] { charge_xyzc[i_charge * 4 + 0], charge_xyzc[i_charge * 4 + 1], charge_xyzc[i_charge * 4 + 2] };
+      for (int j_charge = i_charge; j_charge < n_charge; j_charge++) {
+        const double q_j = charge_xyzc[j_charge * 4 + 3];
+        const double P_j[3] { charge_xyzc[j_charge * 4 + 0], charge_xyzc[j_charge * 4 + 1], charge_xyzc[j_charge * 4 + 2] };
+
+        const double rij[3] = { P_i[0] - P_j[0], P_i[1] - P_j[1], P_i[2] - P_j[2] };
+
+        // Real
+        const std::vector<double3> real_vectors = PeriodicBox::compute_lattice_vectors_within_sphere_real(periodic_parameter.lattice.unit_cell, rij, nuclear_cutoff_real, i_charge == j_charge);
+
+        double dE_real_i[3] { 0.0, 0.0, 0.0 };
+        double dE_real_j[3] { 0.0, 0.0, 0.0 };
+        for (int i_R = 0; i_R < real_vectors.size(); i_R++) {
+          const double3& image_R = real_vectors[i_R];
+          const double r_R[3] = { rij[0] + image_R.x, rij[1] + image_R.y, rij[2] + image_R.z };
+          const double r_2 = r_R[0] * r_R[0] + r_R[1] * r_R[1] + r_R[2] * r_R[2];
+          const double r = sqrt(r_2);
+
+          for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+            dE_real_i[i_xyz] += -r_R[i_xyz] / r_2 / r * erfc(omega * r) - 2.0 / sqrt(M_PI) * omega * r_R[i_xyz] / r_2 * exp(-omega * omega * r_2);
+            dE_real_j[i_xyz] -= -r_R[i_xyz] / r_2 / r * erfc(omega * r) - 2.0 / sqrt(M_PI) * omega * r_R[i_xyz] / r_2 * exp(-omega * omega * r_2);
+          }
+        }
+        if (i_charge == j_charge) {
+          for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+            dE_real_i[i_xyz] /= 2.0;
+            dE_real_j[i_xyz] /= 2.0;
+          }
+        }
+        for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+          dE_real[i_charge * 3 + i_xyz] += dE_real_i[i_xyz] * q_i * q_j;
+          dE_real[j_charge * 3 + i_xyz] += dE_real_j[i_xyz] * q_i * q_j;
+        }
+
+        // Reciporcal
+        double dE_reciprocal_i[3] { 0.0, 0.0, 0.0 };
+        double dE_reciprocal_j[3] { 0.0, 0.0, 0.0 };
+        for (int i_G = 0; i_G < reciprocal_vectors.size(); i_G++) {
+          const double3& image_G = reciprocal_vectors[i_G];
+          const double G[3] { image_G.x, image_G.y, image_G.z };
+          const double G_2 = image_G.x * image_G.x + image_G.y * image_G.y + image_G.z * image_G.z;
+          const double G_dot_delta_R = rij[0] * image_G.x + rij[1] * image_G.y + rij[2] * image_G.z;
+
+          for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+            dE_reciprocal_i[i_xyz] += -exp( -0.25 / (omega * omega) * G_2 ) / G_2 * G[i_xyz] * sin(G_dot_delta_R);
+            dE_reciprocal_j[i_xyz] -= -exp( -0.25 / (omega * omega) * G_2 ) / G_2 * G[i_xyz] * sin(G_dot_delta_R);
+          }
+        }
+        if (i_charge == j_charge) {
+          for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+            dE_reciprocal_i[i_xyz] /= 2.0;
+            dE_reciprocal_j[i_xyz] /= 2.0;
+          }
+        }
+        for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+          dE_reciprocal[i_charge * 3 + i_xyz] += dE_reciprocal_i[i_xyz] * q_i * q_j;
+          dE_reciprocal[j_charge * 3 + i_xyz] += dE_reciprocal_j[i_xyz] * q_i * q_j;
+        }
+
+      }
+    }
+
+    for (int i_charge = 0; i_charge < n_charge; i_charge++) {
+      for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+        dE_real[i_charge * 3 + i_xyz] *= full_range_scale;
+        dE_reciprocal[i_charge * 3 + i_xyz] *= (full_range_scale + long_range_scale) * 4.0 * M_PI / periodic_parameter.lattice.V_real;
+      }
+    }
+
+    for (int i_charge = 0; i_charge < n_charge; i_charge++) {
+      for (int i_xyz = 0; i_xyz < 3; i_xyz++) {
+        gradient[i_charge * 3 + i_xyz] +=       dE_real[i_charge * 3 + i_xyz];
+        gradient[i_charge * 3 + i_xyz] += dE_reciprocal[i_charge * 3 + i_xyz];
+      }
+    }
+
+    delete[] dE_real;
+    delete[] dE_reciprocal;
   }
 }
